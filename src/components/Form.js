4233ea61b40fe34bca8b55cf8fb2f6b919aec363
Josseline <guardadojosseline@gmail.com>
2021-12-19 12:53:59 -0500

    App fully Working using npm start, but not working with gh-page

--- src/components/Form.js
+++ src/components/Form.js
@@ -19,16 +19,13 @@ class Form extends Component {
 
        // update the state everytime the form changes
        this.setState({[e.target.name] : e.target.value})
-         
-      
+
     }
+    onFormSubmit = (e)=>{
 
-    onFormSubmit = (e) => {
-
-        // to prevent page reload on form submit
         e.preventDefault();
 
-        fetch('https://api.openweathermap.org/data/2.5/weather?q='+ this.state.City  + '&appid=' + process.env.REACT_APP_OPEN_WEATHER_API_KEY)
+       fetch('http://api.openweathermap.org/data/2.5/weather?q='+ this.state.City  + '&appid=' + process.env.REACT_APP_OPEN_WEATHER_API_KEY)
         .then(response => response.json())
         .then((City) => {
         this.setState({Location: City.name, temperature: Math.round((City.main.temp-273.15)*1.8+32)+"ºF", descr: City.weather[0].description, wind: Math.round(City.wind.speed * 2.236936) + "mph"})
