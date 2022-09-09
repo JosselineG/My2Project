1874996cb5bf88c1989850b80eb284274e8596b0
Josseline <guardadojosseline@gmail.com>
2022-09-09 13:11:47 -0400

    updating

--- src/components/Form.js
+++ src/components/Form.js
@@ -27,7 +27,7 @@ class Form extends Component {
 
         e.preventDefault();
 
-       fetch('http://api.openweathermap.org/data/2.5/weather?q='+ this.state.City  + '&appid=' + process.env.REACT_APP_OPEN_WEATHER_API_KEY)
+       fetch('https://api.openweathermap.org/data/2.5/weather?q='+ this.state.City  + '&appid=' + process.env.REACT_APP_OPEN_WEATHER_API_KEY)
         .then(response => response.json())
         .then((City) => {
         this.setState({Location: City.name, temperature: Math.round((City.main.temp-273.15)*1.8+32)+"ºF", descr: City.weather[0].description, wind: Math.round(City.wind.speed * 2.236936) + "mph"})
