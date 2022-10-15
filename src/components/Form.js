c2010ef05ca559e5d6c86a06edecf9747f5db46e
Josseline <guardadojosseline@gmail.com>
2022-10-15 01:29:46 -0400

    Working API, need to fix Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,58 +1,65 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Button from '@mui/material/Button'
+import axios from "axios";
 import PublishIcon from '@mui/icons-material/Publish';
-import axios from "axios"; 
-
-function Form(){
-const [info, setInfo] = useState ({
-    City:"", 
-    Location: "", 
-    temperature: "", 
-    descr: "", 
-    wind: "", 
-    Icon: ""});
+import { useState } from "react";
+function Form() {
 
 
+    //Declaring string values
+    const [Info, setInfo] = useState({Location: '',Temperature: '',Description: '',Wind: ''});
+    const [Name, setName] = useState({N:''});
+ 
 
-    const handleChange=(e)=>{
+
+    const handleChange = (e) => {
 
         e.preventDefault();
-        setInfo({...info,[e.target.name]: e.target.value})
+
+        setInfo({...Info, [e.target.name]: e.target.value })
+        setName({...Name, [e.target.name]: e.target.value })
     
     }
-    
-  
 
-    const handleSubmit = async (e)=>{
+
+
+    //The word “async” before a function means: a function always returns a promise.
+    const handleSubmit = async (e) => {
 
         e.preventDefault();
-  
-        
+
+        //The keyword await makes JavaScript wait until that promise settles and returns its result.
       
-      await axios.post('http://localhost:5000/',info) //posting new data from client to apiserver
-    
+        await axios.post('http://localhost:5000/',Name) //posting new data from client to api server
    
-        .then(function(response){
-                console.log(response)
-             
-            }) 
-        
-        .catch(function(error) {
-                console.log(error);
-            });
+            .then((response) => response)
+            .catch(error => console.log(error));
+
+          
+        await axios.get('http://localhost:5000/api') //getting response/data from api sever which will return to client.
+
+            .then((response) => {
+                setInfo({Location: response.data.Lo,
+                         Temperature: response.data.Te,
+                         Description: response.data.De + "ºF",
+                         Icons: <img alt="" src={`https://openweathermap.org/img/w/${response.data.Ic}.png`}/>, 
+                         Wind: "wind: " + response.data.Wi + " mph",
+                        })
+            })
+          //- this will alert the user when they insert the wrong input-
+          .catch((error) => alert("Wrong input, try again! "));
 
     }
 
-  
 
         return(
             <form>
                 <input
                  className='favInput'
                   type = "text" 
-                  name= "City"
+                  name= "N"
                   placeholder='Favorite City...  '
-                  value = {info.City}
+                  value = {Name.N}
                   onChange = {handleChange}
                 />
                  
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,112 +1,110 @@
 import React from 'react';
 import Button from '@mui/material/Button'
-import axios from "axios"; 
+import axios from "axios";
 import { useState } from "react";
-function Home(){
+function Home() {
+
+
+    //Declaring string values
+    const [Info, setInfo] = useState({Location: '',Temperature: '',Description: '',Wind: '',Icons: '' });
+    const [Name, setName] = useState({N:''});
  
-   
-        
-        const [info, setInfo] = useState ({
-        City:"", 
-        Location: "", 
-        temperature: "", 
-        descr: "", 
-        wind: "", 
-        Icon: ""});
 
-   
 
-    const handleChange=(e)=>{
+    const handleChange = (e) => {
 
         e.preventDefault();
-        setInfo({...info,[e.target.name]: e.target.value})
+
+        setInfo({...Info, [e.target.name]: e.target.value })
+        setName({...Name, [e.target.name]: e.target.value })
     
     }
-    
 
 
-    
-    const handleSubmit = async (e)=>{
+
+    //The word “async” before a function means: a function always returns a promise.
+    const handleSubmit = async (e) => {
 
         e.preventDefault();
-  
-        
+
+        //The keyword await makes JavaScript wait until that promise settles and returns its result.
       
-      await axios.post('http://localhost:5000/',info) //posting new data from client to apiserver
-    
+        await axios.post('http://localhost:5000/',Name) //posting new data from client to api server
    
-        .then(function(response){
-                console.log(response)
-             
-            }) 
-        
-        .catch(function(error) {
-                console.log(error);
-            });
+            .then((response) => response)
+            .catch(error => console.log(error));
 
+          
+        await axios.get('http://localhost:5000/api') //getting response/data from api sever which will return to client.
 
-       /*   fetch('http://localhost:5000/api')
-        
-        .then(response => response.json())
-        
-        
-        .then((data) => setInfo({Location:  data.name, temperature: Math.round((data.main.temp-273.15)*1.8+32)+"ºF", descr: data.weather[0].description, Icon: <img alt="" src={`https://openweathermap.org/img/w/${data.weather[0].icon}.png`} />, wind: "wind: "+ Math.round((data.wind.speed) * 2.236936) + " mph"}))
-        
-        .catch((error)=> alert("Incorrect City, Try Again!" + error))
-         */
- 
-}
-        
-            return(
-                <div className="home">
-                    
-                    <div className='searchBox'>
-                    
-                    <input type='text'
-                        className="search-bar"
-                        name="City"
-                        placeholder='Search City...  '
-                        onChange = {handleChange}
-                        value= {info.City}
-                    
-                    />
+            .then((response) => {
+                setInfo({Location: response.data.Lo,
+                         Temperature: response.data.Te,
+                         Description: response.data.De + "ºF",
+                         Icons: <img alt="" src={`https://openweathermap.org/img/w/${response.data.Ic}.png`}/>, 
+                         Wind: "wind: " + response.data.Wi + " mph",
+                        })
+            })
+          //- this will alert the user when they insert the wrong input-
+          .catch((error) => alert("Wrong input, try again! "));
 
-                    <Button 
-                        className= "bt"
-                        style={{backgroundColor: 'black'}} 
-                        size = "small" 
-                        variant='contained' 
-                        onClick={handleSubmit} 
-                        >Submit
-                    </Button>
+    }
 
+    return (
+        <form className="home">
+    
+            <div className='searchBox'>
+
+                <input type='text' required
+                    className="search-bar"
+                    name="N"
+                    placeholder='Search City...  '
+                    onChange={handleChange}
+                    value={Name.N}
+                   
+                   
+                />
+
+                <Button
+                    className="bt"
+                    style={{ backgroundColor: 'black' }}
+                    size="small"
+                    variant='contained'
+                    onClick={handleSubmit}
+                >Submit
+                </Button>
+
+            </div>
+
+            <div className='LocationBox'>
+                <div className='location'>
+                    {Info.Location}
+                </div>
+            </div>
+
+            <div className='WeatherBox'>
+                <div className='temperature'>
+                    {Info.Temperature}
+                    <div className='description'>
+                        {Info.Description}{Info.Icons}
                     </div>
 
-                        <div className='LocationBox'>
-                            <div className='location'>
-                                {info.Location}
-                            </div>
-                        </div>
-
-                        <div className='WeatherBox'>
-                            <div className='temperature'>                                      
-                                    {info.temperature}
-                            <div className= 'description'>
-                                    {info.descr}{info.Icon}
-                            </div>
-
-                            <div className= 'windspeed'>
-                                    {info.wind}
-                            </div>
-                                
-                            </div>
-
-                        </div>
+                    <div className='windspeed'>
+                        {Info.Wind}
+                    </div>
 
                 </div>
-            )
 
-        
+            </div>
+
+       
+        </form>
+    )
+
+
 }
 
 export default Home;
+
+
+
